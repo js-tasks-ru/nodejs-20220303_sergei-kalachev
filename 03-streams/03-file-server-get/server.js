b763a8997e911acf64e33a2ad9e32bb362b2da65
Sergei Kalachev <majorklepto33@gmail.com>
2022-03-13 15:18:10 +0000

    Implement task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,24 +1,50 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const {createReadStream} = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = url.pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
-
-  switch (req.method) {
-    case 'GET':
-
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+  if (req.method !== 'GET') {
+    res.statusCode = 501;
+    res.end('Not implemented');
+    return;
   }
+
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathToFile = path.parse(url.pathname);
+
+  if (pathToFile.dir !== '/') {
+    res.statusCode = 400;
+    res.end('Nested folders are not supported');
+    return;
+  }
+
+  if (pathToFile.ext === '') {
+    res.statusCode = 400;
+    res.end('File extension is missing');
+    return;
+  }
+
+  const filepath = path.join(__dirname, 'files', pathToFile.base);
+
+  const readStream = createReadStream(filepath);
+
+  readStream.on('error', (err) => {
+    if (err.code === 'ENOENT') {
+      res.statusCode = 404;
+      res.end('File does not exist');
+      return;
+    }
+
+    res.statusCode = 500;
+    res.end('Server error');
+    return;
+  }).pipe(res);
+
+  req.on('aborted', () => {
+    readStream.destroy();
+  });
 });
 
 module.exports = server;
