e67f492797eccd2b529a6667e1f9fea29419de34
Sergei Kalachev <majorklepto33@gmail.com>
2022-03-30 10:52:31 +0000

    Implement 06/01-schema-model

--- 06-mongodb-mongoose/01-schema-model/models/Product.js
+++ 06-mongodb-mongoose/01-schema-model/models/Product.js
@@ -1,7 +1,30 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
+const {Schema} = mongoose;
 
 const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  subcategory: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  images: [String],
 });
 
 module.exports = connection.model('Product', productSchema);
