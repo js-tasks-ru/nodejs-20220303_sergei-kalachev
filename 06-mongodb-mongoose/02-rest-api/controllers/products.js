2c861d8f35cd5d62c942225b813fb6e03e09e524
Sergei Kalachev <majorklepto33@gmail.com>
2022-04-06 07:50:26 +0000

    implement 02-rest-api

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,37 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  if (!mongoose.isValidObjectId(subcategory)) {
+    throw new Error('Invalid subcategory id');
+  }
+
+  const products = await Product.find({'subcategories._id': subcategory});
+
+  ctx.body = {products: products.map(mapProduct)};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find({});
+
+  ctx.body = {
+    products: products.map(mapProduct),
+  };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  if (!ctx.params.id) {
+    throw new Error('No product id provided');
+  }
 
+  const product = await Product.findById(ctx.params.id);
+
+  ctx.body = {
+    product: product ? mapProduct(product) : null,
+  };
+};
