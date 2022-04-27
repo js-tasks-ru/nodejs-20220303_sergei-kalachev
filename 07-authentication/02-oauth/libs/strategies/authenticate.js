8901577b930a4a6268f966d5bcac552ec8b11a4e
Sergei Kalachev <majorklepto33@gmail.com>
2022-04-27 10:54:17 +0200

    Implemented 07/02 task

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,21 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  try {
+    if (!email) {
+      done(null, false, `Не указан email`);
+      return;
+    }
+
+    let user = await User.findOne({email});
+
+    if (!user) {
+      user = new User({email, displayName});
+      await user.save();
+    };
+
+    done(null, user);
+  } catch (err) {
+    done(err, false);
+  }
 };
